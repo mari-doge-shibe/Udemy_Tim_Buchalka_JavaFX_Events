a2f423103e64ba406e19fa7db8bc329a6574fc8e
mari_s <ms.mari.suzuki@gmail.com>
2020-05-18 22:05:46 +0900

    started 193.Events Continued - added an argument ActionEvent e

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
 
 public class Controller {
     @FXML
     private TextField nameField;
-    
+
     @FXML
-    public void onButtonClicked() {
+    public void onButtonClicked(ActionEvent e) {
         System.out.println("Hello, " + nameField.getText());
+        System.out.println("The following button was pressed: " + e.getSource());
     }
 }
