fa1861b68db4764f3c2f3303c442c448baadc435
mari_s <ms.mari.suzuki@gmail.com>
2020-05-18 22:00:01 +0900

    in progress 192.Events and Event Handlers - added a text field and associate with an event

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField nameField;
 
     public void onButtonClicked() {
-        System.out.println("Hello");
+        System.out.println("Hello, " + nameField.getText());
     }
 }
