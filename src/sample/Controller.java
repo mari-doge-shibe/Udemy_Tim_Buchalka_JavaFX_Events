7d386744e6b15d84b43d3d57c982ac466a97a936
mari_s <ms.mari.suzuki@gmail.com>
2020-05-18 22:10:19 +0900

    in progress 193.Events Continued - added helloButton and byButton and associated with onButtonClicked

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,16 +2,24 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 
 public class Controller {
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
     @FXML
     public void onButtonClicked(ActionEvent e) {
-        System.out.println("Hello, " + nameField.getText());
-        System.out.println("The following button was pressed: " + e.getSource());
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }
