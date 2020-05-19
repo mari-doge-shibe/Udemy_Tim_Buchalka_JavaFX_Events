dce7b424908105128e4da584cd721874a2e8097e
mari_s <ms.mari.suzuki@gmail.com>
2020-05-19 21:02:28 +0900

    in progress195.Threads and Runnable - add a label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 
@@ -16,6 +17,8 @@ public class Controller {
     private Button byeButton;
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize() {
